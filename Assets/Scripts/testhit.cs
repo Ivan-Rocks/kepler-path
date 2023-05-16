a31ffa1231c1f4685099d57f72ab2b63d606e22c
Ivan-Rocks <13611041610@163.com>
2023-05-16 00:08:16 -0500

    selection finally done

--- Assets/Scripts/testhit.cs
+++ Assets/Scripts/testhit.cs
@@ -1,9 +1,15 @@
+using Microsoft.MixedReality.Toolkit.Input;
+using Microsoft.MixedReality.Toolkit;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using Microsoft.MixedReality.Toolkit.UI;
 
 public class testhit : MonoBehaviour
 {
+
+    public GameObject right;
     public void f()
     {
         print("hi");
@@ -15,9 +21,36 @@ public class testhit : MonoBehaviour
         
     }
 
+    public void OnPointerClicked(MixedRealityPointerEventData eventData)
+    {
+        // Get the position and rotation of the pointer click
+        Vector3 clickPosition = eventData.Pointer.Position;
+        Quaternion clickRotation = eventData.Pointer.Rotation;
+
+        // Perform actions based on the pointer click
+        // ...
+
+        print(clickPosition);
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+        Transform x = right.GetComponent<MRTKRayInteractor>().rayOriginTransform;
+        if (right.GetComponent<MRTKRayInteractor>().isSelectActive && x != null)
+        {
+            Vector3 rayDirection = x.forward;
+            RaycastHit hit;
+            if (Physics.Raycast(x.position, rayDirection, out hit, 30))
+            {
+                // An object is hit along the raycast path
+                GameObject hitObject = hit.collider.gameObject;
+
+                // Use the hit object for further calculations or actions
+                // ...
+
+                Debug.Log("Object Hit: " + hitObject.name);
+            }
+        }
     }
 }
