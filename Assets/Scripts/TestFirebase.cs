0feb541c06a5fa63dc09ffafe9f0c0f76415e448
Ivan-Rocks <13611041610@163.com>
2023-10-27 16:41:01 -0500

    try connect to DB

---
+++ Assets/Scripts/TestFirebase.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestFirebase : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+
+    [DllImport("__Internal")]
+    public static extern void GetJSON(string path, string objectName, string callback, string fallback);
+
+    private void Start()
+    {
+        GetJSON("example", gameObject.name, "OnRequestSuccess", "OnRequestFailed");
+    }
+
+    private void OnRequestSuccess(string data)
+    {
+        text.text = data;
+        text.color = Color.green;
+    }
+
+    private void OnRequestFailed(string data)
+    {
+        text.text = data;
+        text.color = Color.red;
+    }
+}
